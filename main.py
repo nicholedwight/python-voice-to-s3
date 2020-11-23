d69ae87357f30d61bbb5b0afae39b5d53b34bcf7
root <root@DESKTOP-QU1IG9C.localdomain>
2020-11-23 15:05:01 -0700

    added comments for steps

--- main.py
+++ main.py
@@ -15,21 +15,27 @@ def main():
     get_message_store()
 
 def get_message_store():
+    # I had issues using my accountId and extensionId in this request, I'll investigate furhter
     response = platform.get(f'/restapi/v1.0/account/~/extension/~/message-store')
-    print(response.json().records)
+    # print(response.json().records) <-- use this to see how many records/voicemails/texts/faxes exist, for 1 VM it should be 2 records 
     for record in response.json().records:
-        print(record.attachments)
+        # All records have attachments which is the actual data, we're going to check the type for VM
         for attachment in record.attachments:
-            # There are audiorecording and audio transcription attachment types, we're only interested in the recording
+            # There are audiorecording and audio transcription attachment types for VM, we're only interested in the recording
             if attachment.type == "AudioRecording":
-                print(attachment.contentType)
                 if record.type == "VoiceMail":
+                    # Setting a filename to use when we fetch the content from attachment uri
                     fileName = ("vm-%s.id.mp3" % record.attachments[0].id)
 
             # Now we're going to save the mp3
+
+            # Fetch the voicemail content - can be audio recording or transcription 
             content = platform.get(f'{attachment.uri}')
+            # open/create a file with previously defined name, wb because the content is binary
             file = open(("%s" % fileName), "wb")
+            # write content from get response to the file
             file.write(content.body())
+            # close and save!
             file.close
 
 main()
